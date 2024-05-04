f678d41cb40d1ae9643ed17fe6e8c24f50ef5b28
EdwinZarateP <emzp1994@gmail.com>
2024-05-04 17:52:40 -0500

    Uso de handlebars

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 // con esto creamos el servidor y lo guardamos en la variable app para luego exportarlo
 const express = require('express');
-
+const exphbs = require('express-handlebars'); //lo llame exphbs porque es un abreviado de express handlebars
 const path = require('path');
 
 //-------------------------------------------------------------------------------
@@ -11,10 +11,19 @@ const app  = express();
 //Settings o configuraciones: son lo que quiero qe haga express basado en algunos modulo
 app.set('port', process.env.PORT||4000)
 
-
 //__dirname es el path del sistema que se concatena con el views en este caso
 //el path lo debes importar y funciona para anteponer un slash de windows o linux antes del views
 app.set('views', path.join(__dirname,'views')) 
+
+//
+app.engine('.hbs', exphbs.engine({
+    defaultLayout:'main',
+    LayoutsDir: path.join(app.get('views'), 'layouts'),
+    PartialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'  
+}));
+app.set('view engine','.hbs')
+
 
 //-------------------------------------------------------------------------------
 //Middlewares: que son funciones que se van ejecutando a medida que llegan peticiones
@@ -27,7 +36,7 @@ app.use(express.urlencoded({extended:false}));
 //Routes: que son las rutas
 
 app.get('/', (req, res)=>{
-    res.send('Hola todos perros');
+    res.render('index');
 })
 
 //-------------------------------------------------------------------------------
